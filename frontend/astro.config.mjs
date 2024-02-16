a428253d5fe73af434f280007766db789934ade6
oscarprdev <vlcoscar8@gmail.com>
2024-02-16 14:29:28 +0100

    Add vercel adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    integrations: [vue()]
+    integrations: [vue()],
+    adapter: vercel()
 });
