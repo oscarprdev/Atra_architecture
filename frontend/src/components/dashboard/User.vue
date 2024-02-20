67c9ab55a3ab5786b00a7c4947f16277e7e5ddde
oscarprdev <vlcoscar8@gmail.com>
2024-02-20 09:46:01 +0100

    Add dashboard header

---
+++ frontend/src/components/dashboard/User.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { API_URL } from '../../constants';
+import type { User } from '../../api';
+
+const user = ref<User | null>(null);
+
+onMounted(async () => {
+    const response = await fetch(`${API_URL}/user/describe`);
+    const jsonResponse = await response.json();
+
+    user.value = jsonResponse.data;
+});
+</script>
+
+<template>
+    <section>
+        <h1>User</h1>
+    </section>
+</template>
+
+<style>
+section {
+    box-sizing: border-box;
+    height: 100vh;
+}
+</style>
