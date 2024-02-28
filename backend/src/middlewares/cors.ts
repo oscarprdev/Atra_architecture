f04b502d5354fe2cd62bdc81fa345120bd9a13bc
oscarprdev <vlcoscar8@gmail.com>
2024-02-28 15:30:03 +0100

    Modify cors middleware

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -1,27 +1,35 @@
 import { Env } from '..';
 
 const corsHeaders = {
-	'Access-Control-Allow-Origin': 'http://localhost:4321, https://atra-architectura.vercel.app',
+	'Access-Control-Allow-Origin': '',
 	'Access-Control-Allow-Methods': 'GET, OPTIONS, POST, PUT, DELETE',
 	'Access-Control-Allow-Headers': '*',
 };
+const allowedOrigins = ['http://localhost:4321', 'https://atra-architectura.vercel.app'];
 
 const corsMiddleware = (handler: (request: Request, env: Env) => Promise<Response>) => {
 	return async (request: Request, env: Env): Promise<Response> => {
+		const origin = request.headers.get('Origin');
+
+		const headers = { ...corsHeaders };
+		if (origin && allowedOrigins.includes(origin)) {
+			headers['Access-Control-Allow-Origin'] = origin;
+		}
+
 		if (request.method === 'OPTIONS') {
 			return new Response(null, {
 				status: 204,
-				headers: corsHeaders,
+				headers,
 			});
 		}
 
-		const { body, status, headers } = await handler(request, env);
+		const { body, status, headers: responseHeaders } = await handler(request, env);
 
 		const updatedResponse = new Response(body, {
 			status,
 			headers: {
+				...responseHeaders,
 				...headers,
-				...corsHeaders,
 			},
 		});
 
